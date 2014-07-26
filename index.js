e1b6b488201a4d3a4c9875b44a34d55963fa189b
Vincent Lark <vincent.lark@gmail.com>
2014-07-26 15:14:01 +0200

    first version

---
+++ index.js
@@ -0,0 +1,91 @@
+/*!
+ * fasm
+ * https://github.com/vincent/fasm
+ *
+ * Copyright 2014 Vincent Lark
+ * Released under the MIT license
+ */
+/*jshint onevar: false, indent:4 */
+/*global process, console, module, require, define */
+(function () {
+    'use strict';
+    
+    var fasm = {};
+
+    // global on the server, window in the browser
+    var root;
+
+    var _          = require('lodash');
+    var util       = require('util');
+    var colors     = require('colors');
+    
+    var extractor  = require('./lib/extractor');
+    var AssetsFile = require('./lib/assetsfile');
+    
+    var db         = new AssetsFile('./assets.json');
+
+    function credits () {
+        _.map(db.data.assets, function (asset) {
+            console.log(' ', util.format('%s from %s', asset.title.green, asset.canonicalLink.white.bold));
+        });
+    }
+
+    // AssetsCommand
+
+    var command = require('commander');
+
+    command
+        .version(require('./package').version)
+        .option('-v, --peppers', 'Add peppers')
+        .parse(process.argv);
+
+    if (! command.args.length) {
+        command.help();
+        process.exit(1);
+    }
+
+    for (var i = 0; i < command.args.length; i++) {
+
+        var arg   = command.args[i];
+        var next  = (command.args.length > i + 1) ? command.args[i + 1] : null;
+
+        switch (arg) {
+
+            case 'add':
+            case 'install':
+                // if we have an url
+                extractor({ url: next }, db.add.bind(db));
+            break;
+
+            case 'del':
+            case 'uninstall':
+                db.remove(next);
+            break;
+
+            case 'list':
+            case 'credits':
+                credits();
+            break;
+
+
+        }
+        
+    }
+
+
+    // Node.js
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = fasm;
+    }
+    // AMD / RequireJS
+    else if (typeof define !== 'undefined' && define.amd) {
+        define([], function () {
+            return fasm;
+        });
+    }
+    // included directly via <script> tag
+    else {
+        root.fasm = fasm;
+    }
+
+}());
